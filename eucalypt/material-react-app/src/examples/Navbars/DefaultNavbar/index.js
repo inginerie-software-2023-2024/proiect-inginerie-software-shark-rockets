f0f92dbf24e3a347411fdcb582e6efd56f83ee51
Katherine456719 <alexandra.lung@s.unibuc.ro>
2023-12-10 13:16:28 +0200

    Fix logo in browser tab

--- eucalypt/material-react-app/src/examples/Navbars/DefaultNavbar/index.js
+++ eucalypt/material-react-app/src/examples/Navbars/DefaultNavbar/index.js
@@ -13,10 +13,10 @@
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  */
 
-import {useState, useEffect, useContext} from "react";
+import { useState, useEffect, useContext } from "react";
 
 // react-router components
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // prop-types is a library for typechecking of props.
 import PropTypes from "prop-types";
@@ -39,18 +39,18 @@ import DefaultNavbarMobile from "examples/Navbars/DefaultNavbar/DefaultNavbarMob
 import breakpoints from "assets/theme/base/breakpoints";
 
 // Material Dashboard 2 React context
-import {useMaterialUIController} from "context";
-import {AuthContext} from "context";
+import { useMaterialUIController } from "context";
+import { AuthContext } from "context";
 
-function DefaultNavbar({transparent, light, action}) {
+function DefaultNavbar({ transparent, light, action }) {
     const authContext = useContext(AuthContext);
     const [controller] = useMaterialUIController();
-    const {darkMode} = controller;
+    const { darkMode } = controller;
 
     const [mobileNavbar, setMobileNavbar] = useState(false);
     const [mobileView, setMobileView] = useState(false);
 
-    const openMobileNavbar = ({currentTarget}) => setMobileNavbar(currentTarget.parentNode);
+    const openMobileNavbar = ({ currentTarget }) => setMobileNavbar(currentTarget.parentNode);
     const closeMobileNavbar = () => setMobileNavbar(false);
 
     useEffect(() => {
@@ -84,12 +84,12 @@ function DefaultNavbar({transparent, light, action}) {
                 variant="gradient"
                 bgColor="info"
                 py={1}
-                px={{xs: 4, sm: transparent ? 2 : 3, lg: transparent ? 0 : 2}}
+                px={{ xs: 4, sm: transparent ? 2 : 3, lg: transparent ? 0 : 2 }}
                 my={3}
                 mx={3}
                 width="calc(100% - 48px)"
                 height={"3em"}
-                fontSize = "50"
+                fontSize="50"
                 borderRadius="lg"
                 shadow={transparent ? "none" : "md"}
                 color={light ? "white" : "dark"}
@@ -100,33 +100,40 @@ function DefaultNavbar({transparent, light, action}) {
                 left={0}
                 zIndex={3}
                 sx={({
-                         palette: {transparent: transparentColor, white, background},
-                         functions: {rgba},
-                     }) => ({
+                    palette: { transparent: transparentColor, white, background },
+                    functions: { rgba },
+                }) => ({
                     backgroundColor: transparent
                         ? transparentColor.main
                         : rgba(darkMode ? background.sidenav : white.main, 0.8),
                     backdropFilter: transparent ? "none" : `saturate(200%) blur(30px)`,
                 })}
             >
-                <MDBox display={{lg: "flex"}}
-                       height={"100%"}
-                       alignItems="center"  // Center vertically
-                       justifyContent="center"  // Center horizontally
+                <MDBox display={{ lg: "flex" }}
+                    height={"100%"}
+                    alignItems="center"  // Center vertically
+                    justifyContent="center"  // Center horizontally
+                    sx={{ display: "flex" }}
                 >
-                    <Icon sx={{fontSize: 70}}>
-                        <img
-                            src={eucalypt_logo}
-                            alt="Eucalypt Logo"
-                            style={{width: "2em", height: "1.5em", display: "block"}}
-                        />
-                    </Icon>
+                    <MDBox
+                        py={transparent ? 1.5 : 0.75}
+                        lineHeight={1}
+                        pl={{ xs: 0, lg: 1 }}
+                    >
+                        <Icon sx={{ fontSize: 30 }}>
+                            <img
+                                src={eucalypt_logo}
+                                alt="Eucalypt Logo"
+                                style={{ with: "100%", height: "100%", display: "block" }}
+                            />
+                        </Icon>
+                    </MDBox>
                     <MDBox
                         component={Link}
                         to="/"
                         py={transparent ? 1.5 : 0.75}
                         lineHeight={1}
-                        pl={{xs: 0, lg: 1}}
+                        pl={{ xs: 0, lg: 1 }}
                     >
 
                         <MDTypography variant="button" fontWeight="bold" color={light ? "dark" : "white"}>
@@ -135,14 +142,14 @@ function DefaultNavbar({transparent, light, action}) {
                     </MDBox>
                 </MDBox>
                 {authContext.isAuthenticated && (
-                    <MDBox display={{xs: "none", lg: "flex"}} m={0} p={0}>
+                    <MDBox display={{ xs: "none", lg: "flex" }} m={0} p={0}>
                         <DefaultNavbarLink
                             icon="donut_large"
                             name="dashboard"
                             route="/dashboard"
                             light={light}
                         />
-                        <DefaultNavbarLink icon="person" name="profile" route="/profile" light={light}/>
+                        <DefaultNavbarLink icon="person" name="profile" route="/profile" light={light} />
                         <DefaultNavbarLink
                             icon="account_circle"
                             name="sign up"
@@ -158,7 +165,7 @@ function DefaultNavbar({transparent, light, action}) {
                     </MDBox>
                 )}
                 {!authContext.isAuthenticated && (
-                    <MDBox display={{xs: "none", lg: "flex"}} m={0} p={0}>
+                    <MDBox display={{ xs: "none", lg: "flex" }} m={0} p={0}>
                         <DefaultNavbarLink
                             icon="account_circle"
                             name="register"
@@ -175,19 +182,19 @@ function DefaultNavbar({transparent, light, action}) {
                 )}
 
                 <MDBox
-                    display={{xs: "inline-block", lg: "none"}}
+                    display={{ xs: "inline-block", lg: "none" }}
                     lineHeight={0}
                     py={1.5}
                     pl={1.5}
                     color={light ? "dark" : "white"}
-                    sx={{cursor: "pointer"}}
+                    sx={{ cursor: "pointer" }}
                     onClick={openMobileNavbar}
                 >
                     <Icon fontSize="default">{mobileNavbar ? "close" : "menu"}</Icon>
                 </MDBox>
             </MDBox>
 
-            {mobileView && <DefaultNavbarMobile open={mobileNavbar} close={closeMobileNavbar}/>}
+            {mobileView && <DefaultNavbarMobile open={mobileNavbar} close={closeMobileNavbar} />}
         </Container>
     );
 }

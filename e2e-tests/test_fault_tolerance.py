ffda43e90e1d653fdc9a546d932c614f587b8a03
Airinei Andrei <acairinei17@gmail.com>
2024-01-27 18:33:55 +0200

    speedup fault tolerance

--- e2e-tests/test_fault_tolerance.py
+++ e2e-tests/test_fault_tolerance.py
@@ -7,6 +7,6 @@ def test_master_notices_dead_worker(master,worker):
     worker1.terminate()
     assert_log_chain(
         [master.later_wait_on_log_contains("Health check stream failed: Socket closed")] +
-        [master.later_wait_on_log_contains("Health check stream failed:",timeout=100) for _ in range(6)] +
+        [master.later_wait_on_log_contains("Health check stream failed:",timeout=100) for _ in range(4)] +
         [master.later_wait_on_log_contains("Marking worker as inactive")]
     )

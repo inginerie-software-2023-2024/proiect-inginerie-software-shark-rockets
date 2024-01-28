fb00ced3ffeccef12862c61db58a1a1d1225efbd
Airinei Andrei <acairinei17@gmail.com>
2024-01-28 15:29:31 +0200

    give self quota to run tests

--- e2e-tests/eucalypt_endpoint.py
+++ e2e-tests/eucalypt_endpoint.py
@@ -5,9 +5,16 @@ class APIRequester:
         self.base_url = base_url
         self.headers = headers
 
-    def post(self, endpoint, payload):
+    def post(self, endpoint, payload,headers=None):
         url = f"{self.base_url}/{endpoint}"
-        response = requests.post(url, json=payload, headers=self.headers)
+        effective_headers = self.headers if headers is None else headers
+        response = requests.post(url, json=payload, headers=effective_headers)
+        return response.json()
+    
+    def patch(self, endpoint, payload,headers=None):
+        url = f"{self.base_url}/{endpoint}"
+        effective_headers = self.headers if headers is None else headers
+        response = requests.patch(url, json=payload, headers=effective_headers)
         return response.json()
 
     def get(self, endpoint, headers=None):
@@ -46,10 +53,28 @@ def register(name, email, password):
 def generate_token(id):
     return eucalypt_api.get(f"connection/generateToken/{id}")['id']
 
+def get_profile(id,auth_headers):
+    return eucalypt_api.get(f"users/getUser/{id}",auth_headers)['data']
+
+def update_profile(id,profile,auth_headers):
+    return eucalypt_api.patch(f"users/editUser/{id}",profile,auth_headers)
+
+QUOTA_LIMIT = 1000000
 def get_me(token):
     auth_headers = HEADERS.copy()
     auth_headers["Authorization"] = f"Bearer {token}"
-    return eucalypt_api.get("me", auth_headers)
+    me = eucalypt_api.get("me", auth_headers)
+    id = me['data']['id']
+    
+    profile = get_profile(id,auth_headers)
+    old_quota = profile['quota']
+    if old_quota < QUOTA_LIMIT:
+        profile['quota'] = QUOTA_LIMIT
+        update_profile(id,profile,auth_headers)
+        # new_profile = get_profile(id,auth_headers)
+        # print(f"Adjusted quota from {old_quota} to {new_profile['quota']}")
+    
+    return me
 
 def get_id(email,password):
     jwt = login(email, password)['access_token']
